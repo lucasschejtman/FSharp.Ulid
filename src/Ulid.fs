633be046ab0863f5f3ba3f26edada4beb38f5108
Lucas Schejtman <lucas.schejtman@gmail.com>
2017-09-01 11:30:45 +1200

    Moved AP to own module

--- src/Ulid.fs
+++ src/Ulid.fs
@@ -11,15 +11,17 @@ module internal Extensions =
         member this.GetSequence(min, max) =
             Seq.initInfinite (fun _ -> this.Next(min, max))
 
+module internal Helpers =
+    let (|Positive|NotPositive|) num =
+        if num > 0 then Positive else NotPositive
+
 [<AutoOpen>]
 module Ulid =
     open Extensions
+    open Helpers
 
     let encoding        = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"
     let encodingLength  = 32L
-
-    let (|Positive|NotPositive|) num =
-        if num > 0 then Positive else NotPositive
 
     let concatEncoding chars =
         List.fold (fun acc char -> acc + (encoding.Chars char).ToString()) "" chars
@@ -44,15 +46,18 @@ module Ulid =
         member self.Value with get() = value
 
         override self.ToString() = value
-
+        
         static member private Generate timestamp =
             let timePart    = encodeTime timestamp 10
             let randomPart  = randoms 16 0 31
             List.append timePart randomPart
                 |> concatEncoding
 
+        static member private ToUlid =
+            Ulid.Generate >> Ulid
+
         static member FromTimestamp timestamp =
-            timestamp |> Ulid.Generate |> Ulid
+            timestamp |> Ulid.ToUlid
 
         static member New =
-            DateTime.UnixTime |> Ulid.Generate |> Ulid
+            DateTime.UnixTime |> Ulid.ToUlid
