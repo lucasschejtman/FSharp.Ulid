dc9d2e9fc24e30e72625b20ced6ce09924f7b533
Lucas <lucas.schejtman@gmail.com>
2017-09-05 13:23:17 +1200

    Rename subject -> target in tests

--- test/All.fs
+++ test/All.fs
@@ -7,10 +7,10 @@ open Ulid
 let tests =
   testList "Ulid tests" [
     testCase "Creates a new Ulid of the correct length" <| fun _ ->
-      let subject = Ulid.New.Value
-      Expect.equal subject.Length 26 "Incorrect Ulid length"
+      let target = Ulid.New.Value
+      Expect.equal target.Length 26 "Incorrect Ulid length"
 
     testCase "Creates a new Ulid from timestamp" <| fun _ ->
-      let subject = Ulid.FromTimestamp(1469918176385L).Value.Substring(0, 10)
-      Expect.equal subject "01ARYZ6S41" "Incorrect time part"
+      let target = Ulid.FromTimestamp(1469918176385L).Value.Substring(0, 10)
+      Expect.equal target "01ARYZ6S41" "Incorrect time part"
   ]
